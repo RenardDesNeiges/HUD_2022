e75a1c52df9fb2d5b9cb8a854c407b83b090ad5a
Titouan Renard <titouan.renard@gmail.com>
2022-04-13 19:19:16 +0200

    readable code

--- webapp.py
+++ webapp.py
@@ -4,44 +4,64 @@ import numpy as np
 import geopandas as gpd
 import folium
 import rasterio as rio
+from skimage.transform import resize
 
 
-building_1830_path = 'data/b_1830.gpkg'
-raster_1830_path = 'data/1830_modified.tif'
 
-df = gpd.read_file(building_1830_path)
+
+
 app = Flask(__name__)
 
+def rio_to_folium_coords(rio_coords):
+    bnd = list(rio_coords)
+    bnd = [[bnd[3], bnd[2]],[bnd[1], bnd[0]]]
+    clat = (bnd[0][0] + bnd[1][0])/2
+    clon = (bnd[0][1] + bnd[1][1])/2
+    return bnd, clat, clon
+
+def add_raster(path,subsample=1):
     
-with rio.open(raster_1830_path) as src:
-    map_1830_boundary = src.bounds
-    map_1830_img = src.read(1)
+    with rio.open(path) as src:
+        map_boundary = src.bounds
+        map_img = src.read(1)
 
-bnd = list(map_1830_boundary)
-bnd = [[bnd[3], bnd[2]],[bnd[1], bnd[0]]]
-clat = (bnd[0][0] + bnd[1][0])/2
-clon = (bnd[0][1] + bnd[1][1])/2
+    bnd, clat, clon = rio_to_folium_coords(map_boundary)
+    
+    map_low_res = resize(map_img, (map_img.shape[0]//subsample, map_img.shape[1]//subsample))
+    
+    raster = folium.raster_layers.ImageOverlay(map_low_res,
+                            bnd,
+                            opacity=1)
+    
+    return raster, clat, clon
 
-display_map = folium.Map(location=[clat,clon], 
-                  tiles="Stamen Terrain",
-                  zoom_start=12) 
+def add_shape(path):
+    df = gpd.read_file(path)
+    return folium.GeoJson(data=df['geometry'])
+    
+    
+def build_folium():        
+    
+    building_1830_path = 'data/b_1830.gpkg'
+    raster_1830_path = 'data/1830_modified.tif'
+    
+    raster, clat, clon = add_raster(raster_1830_path,3)
+    shape = add_shape(building_1830_path)
+    display_map = folium.Map(location=[clat,clon], 
+                    tiles="Stamen Terrain",
+                    zoom_start=15) 
 
-# folium.raster_layers.ImageOverlay(map_1830_img,
-#                         bnd,
-#                         opacity=1).add_to(display_map)
-folium.GeoJson(data=df['geometry']).add_to(display_map)
+    raster.add_to(display_map)
+    
+    shape.add_to(display_map)
 
-print("Folium loaded!")
+    print("Folium loaded!")
+    return display_map
+
+display_map = build_folium()
 
 @app.route('/')
 def index():
-
-        
-
-    
-
-
-    
 
     return display_map._repr_html_()
 
