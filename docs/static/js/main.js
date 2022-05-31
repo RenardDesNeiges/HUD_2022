b1a33d294c58a27f176f82ec3d2f422c96f8c528
Titouan Renard <titouan.renard@gmail.com>
2022-05-31 12:57:07 +0200

    cute logos

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -30,12 +30,13 @@ $(document).ready(function () {
     });
     return _icon;
   }
-  bridge_icon = generateIcon("static/icons/bridge.png");
-  boat_icon = generateIcon("static/icons/boat.png");
-  building_icon = generateIcon("static/icons/building.png");
-  horse_icon = generateIcon("static/icons/horse-car.png");
-  metro_icon = generateIcon("static/icons/metro.png");
-  train_icon = generateIcon("static/icons/train.png");
+  tree_icon = generateIcon('static/icons/tree.png')
+  bridge_icon = generateIcon('static/icons/bridge.png')
+  boat_icon = generateIcon('static/icons/boat.png')
+  building_icon = generateIcon('static/icons/building.png')
+  horse_icon = generateIcon('static/icons/horse-car.png')
+  metro_icon = generateIcon('static/icons/metro.png')
+  train_icon = generateIcon('static/icons/train.png')
 
   var map = L.map("map").setView([46.51924129322557, 6.6315754917602625], 14);
 
@@ -238,32 +239,27 @@ $(document).ready(function () {
       });
     });
 
-  //marker images 1830
-  var vallee_flon_img =
-    '<img src="static/img/flon_1844_7.jpeg" class="w-screen"/>';
-  var diligences_img =
-    '<img src="static/img/hotel_des_postes.jpeg" class="w-screen"/>';
-  //marker images 1873
-  var gare_1873_img =
-    '<img src="static/img/Inaug_gare_lausanne.jpg" class="w-screen"/>';
-  var ouchy_1873_img =
-    '<img src="static/img/beau_rivage.jpeg" class="w-screen"/>';
-  //marker images 1894
-  var gare_1894_img = '<img src="static/img/gare_1900.jpg" class="w-screen"/>';
-  var ouchy_1894_img =
-    '<img src="static/img/ouchy_port_1.jpg" class="w-screen"/>';
-  var funiculaire_1894_img =
-    '<img src="static/img/ligne_funiculaire.jpeg" class="w-screen"/>';
-  var flon_1894_img =
-    '<img src="static/img/marchandises_flon.jpg" class="w-screen"/>';
-  var depot_TL_1894_img =
-    '<img src="static/img/generatrice.jpg" class="w-screen"/>';
-  var liberte_1894_img =
-    '<img src="static/img/promenade_de_la_liberte_1905.jpg" class="w-screen"/>';
-  var sainf_eglise_1894_img =
-    '<img src="static/img/sainf_eglise_1899.jpg" class="w-screen"/>';
+    //marker images 1830
+    var vallee_flon_img = '<img src="static/img/flon_1830.jpeg" class="w-screen"/>';
+    var diligences_img = '<img src="static/img/hotel_des_postes.jpeg" class="w-screen"/>';
+    //marker images 1873
+    var vallee_flon_pont_img = '<img src="static/img/flon_1844_7.jpeg" class="w-screen"/>';
+    var gare_1873_img = '<img src="static/img/Inaug_gare_lausanne.jpg" class="w-screen"/>';
+    var ouchy_1873_img = '<img src="static/img/beau_rivage.jpeg" class="w-screen"/>';
+    //marker images 1894
+    var gare_1894_img = '<img src="static/img/gare_1900.jpg" class="w-screen"/>';
+    var ouchy_1894_img = '<img src="static/img/ouchy_port_1.jpg" class="w-screen"/>';
+    var funiculaire_1894_img = '<img src="static/img/ligne_funiculaire.jpeg" class="w-screen"/>';
+    var flon_1894_img = '<img src="static/img/marchandises_flon.jpg" class="w-screen"/>';
+    var flon_remblaiment_1894_img = '<img src="static/img/flon_remblement.jpg" class="w-screen"/>';
+    var depot_TL_1894_img = '<img src="static/img/generatrice.jpg" class="w-screen"/>';
+    var liberte_1894_img = '<img src="static/img/promenade_de_la_liberte_1905.jpg" class="w-screen"/>';
+    var sainf_eglise_1894_img = '<img src="static/img/sainf_eglise_1899.jpg" class="w-screen"/>';
+    
 
-  // markers
+
+
+    // markers
   const markers = {
     1830: [
       L.marker([46.519549, 6.632527], { icon: horse_icon })
@@ -275,91 +271,87 @@ $(document).ready(function () {
       L.marker([46.50656843687059, 6.6262587784441385], { icon: boat_icon })
         .bindPopup(
           "<b>Débarcadère</b><br>Débarcadère d'Ouchy, arrivée des bâteaux à vapeur à Lausanne.<br><a href='#ouchy'>En lire plus</a>"
-        )
-        .addTo(map),
-      L.marker([46.521708715317914, 6.626355101184518], { icon: bridge_icon })
-        .bindPopup(
-          "<b>Vallée du Flon</b><br>En 1840 la valée du flon n'est pas encore comblée.<br><a href='#vallee_flon'>En lire plus</a>" +
-            vallee_flon_img
-        )
-        .addTo(map),
-    ],
-    1873: [
-      L.marker([46.516526, 6.629209], { icon: train_icon }).bindPopup(
-        "<b>Gare</b><br>Le début des trains à lausanne..<br><a href='#gare'>En lire plus</a>" +
-          gare_1873_img
-      ),
-      L.marker([46.50656843687059, 6.62625877844413858], {
-        icon: boat_icon,
-      }).bindPopup(
-        "<b>Ouchy</b><br>Débarcadère d'Ouchy, arrivée des bâteaux à vapeur à Lausanne.<br><a href='#ouchy_1873'>En lire plus</a>" +
-          ouchy_1873_img
-      ),
-    ],
-    1894: [
-      L.marker([46.512952, 6.628115], { icon: metro_icon }).bindPopup(
-        "<b>Funiculaire</b><br>Funiculaire reliant Ouchy au Flon<br><a href='#funiculaire'>En lire plus</a>" +
-          funiculaire_1894_img
-      ),
-      L.marker([46.50656843687059, 6.6262587784441385], {
-        icon: boat_icon,
-      }).bindPopup(
-        "<b>Débarcadère</b><br>Débarcadère d'Ouchy, arrivée des bâteaux à vapeur à Lausanne et connection avec le Funiculaire.<br><a href='#ouchy_1894'>En lire plus</a>" +
-          ouchy_1894_img
-      ),
-      L.marker([46.52064916988745, 6.629920286708344], {
-        icon: building_icon,
-      }).bindPopup(
-        "<b>Le Flon</b><br>Arrivée du funiculaire au flon<br><a href='#flon_1894'>En lire plus</a>" +
-          flon_1894_img
-      ),
-      L.marker([46.52218500033085, 6.637986421916366], {
-        icon: metro_icon,
-      }).bindPopup(
-        "<b>Dépot des Tramways Lausannois</b><br>Depot et centrale électrique.<br><a href='#tram_HQ'>En lire plus</a>" +
-          depot_TL_1894_img
-      ),
-      L.marker([46.51960369672084, 6.6327074018760905], {
-        icon: metro_icon,
-      }).bindPopup(
-        "<b>Saint François</b><br>Passage du Tramway à Saint-François.<br><a href='#tram_Sainf'>Voir la photo et lire l'article.</a>" +
-          sainf_eglise_1894_img
-      ),
-      L.marker([46.516526, 6.629209], { icon: train_icon }).bindPopup(
-        "<b>Gare</b><br>Développements de la gare de Lausanne.<br><a href='#gare_1894'>En lire plus</a>" +
-          gare_1894_img
-      ),
-      L.marker([46.528844392159506, 6.630193754067561], {
-        icon: metro_icon,
-      }).bindPopup(
-        "<b>Tramway</b><br>Passage d'un tramway devant la promenade de la liberté.<br><a href='#promenade_liberte'>Voir la photo et lire l'article.</a>" +
-          liberte_1894_img
-      ),
-    ],
-    1928: [
-      L.marker([46.518924, 6.634789], { icon: metro_icon }).bindPopup(
-        "<b>Tramway</b><br>Développement des tramways<br><a href='#tram1928'>En lire plus</a>"
-      ),
-    ],
+        ).addTo(map),
+        L.marker([46.52376370339941, 6.617639112903395],{icon: tree_icon}).bindPopup(
+          "<b>Vallée du Flon</b><br>En 1830 la valée du flon n'est pas encore comblée.<br><a href='#vallee_flon'>En lire plus</a>"
+        +vallee_flon_img).addTo(map),
+      ],
+      1873: [
+        L.marker([46.516526, 6.629209],{icon: train_icon}).bindPopup(
+          "<b>Gare</b><br>Le début des trains à lausanne..<br><a href='#gare'>En lire plus</a>" + gare_1873_img
+          ),
+          L.marker([46.50656843687059, 6.62625877844413858],{icon: boat_icon}).bindPopup(
+            "<b>Ouchy</b><br>Débarcadère d'Ouchy, arrivée des bâteaux à vapeur à Lausanne.<br><a href='#ouchy_1873'>En lire plus</a>"+ouchy_1873_img
+            ),
+          L.marker([46.521708715317914, 6.626355101184518],{icon: bridge_icon}).bindPopup(
+            "<b>Vallée du Flon</b><br>En 1873 le comblement de la vallée du flon n'est que partiel.<br><a href='#vallee_flon_1857'>En lire plus</a>"
+          +vallee_flon_pont_img),
+        ],
+        1894: [
+          L.marker([46.512952, 6.628115],{icon: metro_icon}).bindPopup(
+          "<b>Funiculaire</b><br>Funiculaire reliant Ouchy au Flon<br><a href='#funiculaire'>En lire plus</a>"+funiculaire_1894_img
+          ),
+          L.marker([46.50656843687059, 6.6262587784441385],{icon: boat_icon}).bindPopup(
+            "<b>Débarcadère</b><br>Débarcadère d'Ouchy, arrivée des bâteaux à vapeur à Lausanne et connection avec le Funiculaire.<br><a href='#ouchy_1894'>En lire plus</a>"+ouchy_1894_img
+          ),
+          L.marker([46.52064916988745, 6.629920286708344],{icon: building_icon}).bindPopup(
+            "<b>Le Flon</b><br>Arrivée du funiculaire au flon<br><a href='#flon_1894'>En lire plus</a>"+flon_1894_img
+          ),
+          L.marker([46.521708715317914, 6.626355101184518],{icon: bridge_icon}).bindPopup(
+            "<b>Le Flon</b><br>Le flon est remblayé avec le materiau extrait pour la construction du funiculaire.<br><a href='#flon_1894'>En lire plus</a>"+flon_remblaiment_1894_img
+          ),
+          L.marker([46.52218500033085, 6.637986421916366],{icon: metro_icon}).bindPopup(
+            "<b>Dépot des Tramways Lausannois</b><br>Depot et centrale électrique.<br><a href='#tram_HQ'>En lire plus</a>"+depot_TL_1894_img
+          ),
+          L.marker([46.51960369672084, 6.6327074018760905],{icon: metro_icon}).bindPopup(
+            "<b>Saint François</b><br>Passage du Tramway à Saint-François.<br><a href='#tram_Sainf'>Voir la photo et lire l'article.</a>"+sainf_eglise_1894_img
+          ),
+          L.marker([46.516526, 6.629209],{icon: train_icon}).bindPopup(
+            "<b>Gare</b><br>Développements de la gare de Lausanne.<br><a href='#gare_1894'>En lire plus</a>"+gare_1894_img
+          ),
+          L.marker([46.528844392159506, 6.630193754067561],{icon: metro_icon}).bindPopup(
+            "<b>Tramway</b><br>Passage d'un tramway devant la promenade de la liberté.<br><a href='#promenade_liberte'>Voir la photo et lire l'article.</a>"+liberte_1894_img
+            ),
+          ],
+          1928: [
+          L.marker([46.518924, 6.634789],{icon: metro_icon}).bindPopup(
+            "<b>Tramway</b><br>Développement des tramways<br><a href='#tram1928'>En lire plus</a>"
+            ),
+          L.marker([46.516526, 6.629209],{icon: train_icon}).bindPopup(
+              "<b>Gare</b><br>Développements de la gare de Lausanne.<br><a href='#gare_1894'>En lire plus</a>"+gare_1894_img
+            ),
+          L.marker([46.52178535359798, 6.629567871159335],{icon: building_icon}).bindPopup(
+            "<b>Bel-Air</b><br>Ascenceur a train omg c'est absolutely bananas.<br><a href='#bel_air_1928'>En lire plus</a>"
+            ),
+          L.marker([46.52209385399581, 6.625520412507395],{icon: building_icon}).bindPopup(
+            "<b>Entrepots du Flon</b><br>Developpement des entrepots du Flon.<br><a href='#bel_air_1928'>En lire plus</a>"
+            ),
+          L.marker([46.52064916988745, 6.629920286708344],{icon: building_icon}).bindPopup(
+            "<b>Flon</b><br>Le Flon en 1928.<br><a href='#flon_1928'>En lire plus</a>"
+            ),
+          L.marker([46.506665528620374, 6.627924678748705],{icon: building_icon}).bindPopup(
+            "<b>Ouchy</b><br>Arret Ouchy du Tramway<br><a href='#flon_1928'>En lire plus</a>"
+            ),
+          ],
   };
 
   // articles
 
   const articles = {
-    1830: [
-      $("#diligences").show(),
-      $("#ouchy").show(),
-      $("#vallee_flon").show(),
-    ],
-    1873: [$("#gare"), $("#ouchy_1873")],
-    1894: [
-      $("#funiculaire"),
-      $("#tram1896"),
-      $("#flon_1894"),
-      $("#gare_1894"),
-      $("#ouchy_1894"),
-    ],
-    1928: [$("#tram1928")],
+    1830: [ $("#diligences").show(),
+            $("#ouchy").show(),
+            $("#vallee_flon").show()],
+    1873: [ $("#gare"),
+            $("#ouchy_1873"),
+            $("#vallee_flon_1857")],
+    1894: [ $("#funiculaire"), 
+            $("#tram1896"), 
+            $("#flon_1894"), 
+            $("#gare_1894"),
+            $("#ouchy_1894")],
+    1928: [ $("#tram1928"),
+            $("#flon_1928"),
+            $("#bel_air_1928"),],
   };
 
   // gestion des boutons
